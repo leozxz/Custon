e4eacb7dc479c8bd44bca5d37aa86e57ebec6705
Leonardo Andrade <52091203+leozxz@users.noreply.github.com>
2022-08-29 15:22:32 -0300

    Update customActivity.js

--- customActivity.js
+++ customActivity.js
@@ -4,24 +4,30 @@ define(["postmonger"], function (Postmonger) {
   var connection = new Postmonger.Session();
   var payload = {};
   var lastStepEnabled = false;
-  var steps = [
-    // initialize to the same value as what's set in config.json for consistency
-    { label: "Step 1", key: "step1" },
-    { label: "Step 2", key: "step2" },
-    { label: "Step 3", key: "step3" },
-    { label: "Step 4", key: "step4", active: false },
-  ];
-  var currentStep = steps[0].key;
+  var phonenumber = null;
+  var email = null;
+  var email_value = null;
+  var phoneNumber = null;
+  var idade_value = null;
 
+  /* var steps = [
+     // initialize to the same value as what's set in config.json for consistency
+     { label: "Step 1", key: "step1" },
+     { label: "Step 2", key: "step2" },
+     { label: "Step 3", key: "step3" },
+     { label: "Step 4", key: "step4", active: false },
+   ];
+   var currentStep = steps[0].key;
+ */
   $(window).ready(onRender);
 
   connection.on("initActivity", initialize);
   connection.on("requestedTokens", onGetTokens);
   connection.on("requestedEndpoints", onGetEndpoints);
+  connection.on('clickedNext', save);
 
-  connection.on("clickedNext", onClickedNext);
-  connection.on("clickedBack", onClickedBack);
-  connection.on("gotoStep", onGotoStep);
+
+
 
   function onRender() {
     // JB will respond the first time 'ready' is called with 'initActivity'
@@ -30,25 +36,7 @@ define(["postmonger"], function (Postmonger) {
     connection.trigger("requestTokens");
     connection.trigger("requestEndpoints");
 
-    // Disable the next button if a value isn't selected
-    $("#select1").change(function () {
-      var message = getMessage();
-      connection.trigger("updateButton", {
-        button: "next",
-        enabled: Boolean(message),
-      });
 
-      $("#message").html(message);
-    });
-
-    // Toggle step 4 active/inactive
-    // If inactive, wizard hides it and skips over it during navigation
-    $("#toggleLastStep").click(function () {
-      lastStepEnabled = !lastStepEnabled; // toggle status
-      steps[3].active = !steps[3].active; // toggle active
-
-      connection.trigger("updateSteps", steps);
-    });
   }
 
   function initialize(data) {
@@ -59,9 +47,9 @@ define(["postmonger"], function (Postmonger) {
     var message;
     var hasInArguments = Boolean(
       payload["arguments"] &&
-        payload["arguments"].execute &&
-        payload["arguments"].execute.inArguments &&
-        payload["arguments"].execute.inArguments.length > 0
+      payload["arguments"].execute &&
+      payload["arguments"].execute.inArguments &&
+      payload["arguments"].execute.inArguments.length > 0
     );
 
     var inArguments = hasInArguments
@@ -73,21 +61,12 @@ define(["postmonger"], function (Postmonger) {
         if (key === "message") {
           message = val;
         }
+
       });
     });
 
-    // If there is no message selected, disable the next button
-    if (!message) {
-      showStep(null, 1);
-      connection.trigger("updateButton", { button: "next", enabled: false });
-      // If there is a message, skip to the summary step
-    } else {
-      $("#select1")
-        .find("option[value=" + message + "]")
-        .attr("selected", "selected");
-      $("#message").html(message);
-      showStep(null, 3);
-    }
+
+
   }
 
   function onGetTokens(tokens) {
@@ -100,103 +79,34 @@ define(["postmonger"], function (Postmonger) {
     // console.log(endpoints);
   }
 
-  function onClickedNext() {
-    if (
-      (currentStep.key === "step3" && steps[3].active === false) ||
-      currentStep.key === "step4"
-    ) {
-      save();
-    } else {
-      connection.trigger("nextStep");
-    }
-  }
+  var eventDefinitionKey;
+  connection.trigger('requestInteraction');
 
-  function onClickedBack() {
-    connection.trigger("prevStep");
-  }
-
-  function onGotoStep(step) {
-    showStep(step);
-    connection.trigger("ready");
-  }
-
-  function showStep(step, stepIndex) {
-    if (stepIndex && !step) {
-      step = steps[stepIndex - 1];
-    }
-
-    currentStep = step;
-
-    $(".step").hide();
-
-    switch (currentStep.key) {
-      case "step1":
-        $("#step1").show();
-        connection.trigger("updateButton", {
-          button: "next",
-          enabled: Boolean(getMessage()),
-        });
-        connection.trigger("updateButton", {
-          button: "back",
-          visible: false,
-        });
-        break;
-      case "step2":
-        $("#step2").show();
-        connection.trigger("updateButton", {
-          button: "back",
-          visible: true,
-        });
-        connection.trigger("updateButton", {
-          button: "next",
-          text: "next",
-          visible: true,
-        });
-        break;
-      case "step3":
-        $("#step3").show();
-        connection.trigger("updateButton", {
-          button: "back",
-          visible: true,
-        });
-        if (lastStepEnabled) {
-          connection.trigger("updateButton", {
-            button: "next",
-            text: "next",
-            visible: true,
-          });
-        } else {
-          connection.trigger("updateButton", {
-            button: "next",
-            text: "done",
-            visible: true,
-          });
-        }
-        break;
-      case "step4":
-        $("#step4").show();
-        break;
-    }
-  }
-
+  connection.on('requestedInteraction', function (settings) {
+    eventDefinitionKey = settings.triggers[0].metaData.eventDefinitionKey;
+    console.log(eventDefinitionKey);
+  });
   function save() {
-    var name = $("#select1").find("option:selected").html();
-    var value = getMessage();
 
+    var email_value = "{{Contact.Attribute." + eventDefinitionKey + ".Email}}"
+    var telefone_value = "{{Contact.Attribute." + eventDefinitionKey + ".Telefone}}"
+    var nome_value = "{{Contact.Attribute." + eventDefinitionKey + ".Nome}}"
+    var idade_value = "{{Contact.Attribute." + eventDefinitionKey + ".Idade}}"
+    console.log(email_value)
     // 'payload' is initialized on 'initActivity' above.
     // Journey Builder sends an initial payload with defaults
     // set by this activity's config.json file.  Any property
     // may be overridden as desired.
-    payload.name = name;
+    // payload.name = name;
 
-    payload["arguments"].execute.inArguments = [{ message: value }];
+    payload["arguments"].execute.inArguments = [{ email: email_value, Telefone: telefone_value, nome: nome_value, idade: idade_value }];
 
     payload["metaData"].isConfigured = true;
 
     connection.trigger("updateActivity", payload);
+
+
   }
 
-  function getMessage() {
-    return $("#select1").find("option:selected").attr("value").trim();
-  }
+
 });
